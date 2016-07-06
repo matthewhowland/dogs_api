231d5143ddb3b4c74c5d5de38df0d0b1f2f60ef3
Matthew Howland <howlandm@gmail.com>
2016-07-05 20:43:09 -0400

    have post route working

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
+var server = express();
+var uuid = require('uuid');
+
+var port = process.env.PORT || 8080;
+var db = lowdb('db.json');
+
+//Database initialization
+db.defaults({dogs: []})
+  .value(); //runs the previous set of commands
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+
+server.get('/dogs', function(request, response){
+  response.send('/dogs');
+});
+
+server.get('/dogs/:id', function(request, response){
+  response.send('/dogs/:id');
+});
+
+server.post('/dogs', function(request, response){
+  var dogs = {
+    id: uuid.v4(),
+    dogName: request.body.dogName,
+    breed: request.body.breed,
+    size: request.body.size,
+    color: request.body.color,
+    age: request.body.age,
+    gender: request.body.gender,
+    likesDogs: true,
+    likesHumans: true,
+    likesKids: true,
+    wantsPlaymates: true,
+    timesAtPark: request.body.timesAtPark,
+    notes: request.body.notes,
+  };
+
+  var result = db.get('dogs')
+                  .push(dogs)
+                  .last()
+                  .value();
+
+response.send(result);
+});
+
+server.put('/dogs/:id', function(request, response){
+  response.send('/dogs/:id');
+});
+
+server.delete('/dogs/:id', function (request, response){
+  response.send('/dogs/:id');
+});
+
+server.listen(port, function(){
+  console.log('Now listening on port ...', port);
+});
